4d4c4f3180e6781aaf3ef576be9b356920f8f789
heinwaihtet <heinwaihtet@f.myanmardcr.com>
2022-01-25 00:55:06 +0630

    adding signup and login vue

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <h2>Login</h2>
+  <form @submit.prevent="login">
+    <input
+        type="email"
+        v-model="email"
+        placeholder="Email" />
+    <input
+        type="password"
+        v-model="password"
+        placeholder="Password" />
+    <button>Sign up</button>
+  </form>
+</template>
+
+<script>
+import {ref} from "vue"
+export default {
+    setup(){
+        let password = ref("");
+        let email = ref("");
+        let login = () => {
+            console.log(password.value, email.value);
+        }
+        return { password, email, login};
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,37 @@
+<template>
+  <h2>Sign Up</h2>
+  <form @submit.prevent="signUp">
+      <input
+        type="text"
+        v-model="displayName"
+        placeholder="Display Name" />
+    <input
+        type="email"
+        v-model="email"
+        placeholder="Email" />
+    <input
+        type="password"
+        v-model="password"
+        placeholder="Password" />
+    <button>Login</button>
+  </form>
+</template>
+
+<script>
+import {ref} from "vue"
+export default {
+    setup(){
+        let displayName = ref("");
+        let password = ref("");
+        let email = ref("");
+        let signUp = () => {
+            console.log(displayName.value, password.value, email.value);
+        }
+        return {displayName, password, email, signUp};
+    }
+}
+</script>
+
+<style>
+
+</style>
