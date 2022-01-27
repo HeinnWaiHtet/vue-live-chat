99041841479060058d50b6fb1e66a696383a9570
heinwaihtet <heinwaihtet@f.myanmardcr.com>
2022-01-27 20:07:18 +0630

    adding chat window view

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="container">
       <Navbar></Navbar>
+      <ChatWindow></ChatWindow>
       <NewChatForm></NewChatForm>
 </div>
 </template>
 
 <script>
+import ChatWindow from '../components/ChatWindow'
 import NewChatForm from '../components/NewChatForm'
 import Navbar from '../components/Navbar'
 export default {
   components: {
+    ChatWindow,
     NewChatForm, Navbar },
 
 }
